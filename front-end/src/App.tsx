b0a729aaedab8629466eb700881a6636e120585d
bhavyalingaa <122070163+bhavyalingaa@users.noreply.github.com>
2024-02-13 17:34:37 -0800

    Update App.tsx
    added frontend connections

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -10,6 +10,8 @@ interface AppState {
   page: "landing" | "ideation" | "explore"
   subpage: string
   messageFromFlask: string
+  historyMap: HistoryType[]
+  ideaEntries: IdeasEntry[];
 }
 
 
@@ -17,8 +19,54 @@ export default class App extends Component<{}, AppState>{
   constructor(props: {}) {
     super(props);
 
-   this.state ={page:"ideation", subpage:"", messageFromFlask: null,}
+   this.state ={page:"ideation", subpage:"", messageFromFlask: null,historyMap: null,ideaEntries: null}
 }
+
+// add entry
+addEntry = () => {
+  axios.get('/addIdeaEntry?IdeaName=testingAPI&Description=description%20of%20idea&HistoryID=b49d01e1-9d78-41bc-9a49-d037c4747a25')
+    .then(response => {
+      if(response.data.message == 'success'){
+        console.log('Successful');
+      }
+      else{
+        console.log('Unsuccessful');
+      }
+      
+    })
+    .catch(error => {
+      console.error('Error adding entry:', error);
+    });
+};
+
+
+getHistory = () => {
+  axios.get('/getAllHistory')
+    .then(response => {
+      const historyMap: HistoryType[] = response.data;
+        this.setState({ historyMap });
+      
+    })
+    .catch(error => {
+      console.error('Error getting history:', error);
+    });
+};
+
+getAllIdeas = () => {
+  axios.get('/getAllIdeaEntriesForHistory?HistoryID=b49d01e1-9d78-41bc-9a49-d037c4747a25')
+  .then(response => {
+    const ideaEntries: IdeasEntry[] = response.data;
+      this.setState({ ideaEntries });
+  })
+  .catch(error => {
+    console.error('Error getting all ideas:', error);
+  });
+
+}
+
+
+
+
 getData = () => {
   axios.get('/test')
     .then(response => {
