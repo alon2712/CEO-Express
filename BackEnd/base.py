c94701ecb6b084fefa44e8373e8d0ea4dc754115
Tim Kaltenbach <123493421+timkaltenbach@users.noreply.github.com>
2024-03-06 19:34:46 -0800

    Update base.py

--- BackEnd/base.py
+++ BackEnd/base.py
@@ -1,148 +1,164 @@
-import pyodbc
-import os
-import struct
-import uuid
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import databaseInfo
 import json
+from openai import OpenAI
+import time
+api = Flask(__name__)
+CORS(api)
 
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
+# Just an example on how to use the api
+@api.route('/test')
+def tester():
+    response_body = {
+        'message': 'Flask react connection works'
+    }
+    return response_body
 
-class Person(BaseModel):
-    first_name: str
-    last_name: Union[str, None] = None
 
-with open('connectionString.txt', 'r') as file:
-    # Read the contents of the file and store it in a variable
-    file_contents = file.read()
-connection_string = file_contents
+"""
+This endpoint is used to generate ideas and add them to the database. Gets called with an api call like so:
+http://127.0.0.1:5000/getNewIdeas?Ideas=Swimmer,Surfer&Count=2
+"""
+@api.route('/getNewIdeas', methods=['POST'])
+def getNewIdeas():
+    client = OpenAI(
+        api_key=""
+    )
 
-app = FastAPI()
+    ideas = request.json.get('Ideas')
+    count = request.json.get('Count')
+    historyId = request.json.get('HistoryId')
 
-def getAllHistoryQuery():
-    returnList = []
-    conn = get_conn()
-    cursor = conn.cursor()
-    cursor.execute("SELECT HistoryId, Name, UserName FROM History order by CreateDate asc")
-    columns = [column[0] for column in cursor.description]
-    for row in cursor.fetchall():
-        returnList.append(dict(zip(columns, row)))
-    conn.close()
-    return returnList
-
-def getAllIdeaEntriesForHistory(HistoryId):
-    returnList = []
-    conn = get_conn()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM IdeaEntry WHERE HistoryId=? order by CreateDate asc", HistoryId)
-    columns = [column[0] for column in cursor.description]
-    for row in cursor.fetchall():
-        returnList.append(dict(zip(columns, row)))
-    conn.close()
-    return returnList
-
-def addIdeaEntryQuery(ideaName, description, historyId):
-    conn = get_conn()
-    cursor = conn.cursor()
-    domain = str(uuid.uuid4())
+    exc = ""
     try:
-        cursor.execute("INSERT INTO IdeaEntry (Name, Description, DomainName, HistoryId, IsGenerated) VALUES ( ?, ?, ?, ?, 0)", (ideaName, description, str(domain)[:20], historyId)) 
-        conn.commit()
-        conn.close()
-    except Exception as e:
-        print("An error occurred:", e)
-        return e 
+        t0 = time.time()
+           
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": "Imagine that you are a highly imaginative entrepreneur, renowned for your ability to innovate beyond the confines of ordinary thought but also keeping things obtainable and senseble. Your extraordinary talent lies not only in your capacity to conceive wholly original ideas, but also in your ability to take existing concepts and ingeniously transform them into something much more abstract and avant-garde, but also simple and createble by the average human. Curiosity, inventiveness, and a fearless disregard for the expected are virtually imprinted in your DNA. Society's norms and conventional wisdom have no hold over your entrepreneurial spirit and business strategies. Given these ideas, " + ideas + ", generate a new screative possible idea that is different, explaining how it would work behind the scenes, in less than 1 sentence (20 words). Provide only a description (NO TITLE)"}],
+        )
+        t1 = time.time()
+        total1 = t1-t0
+        completion2 = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": "Given this description :" + completion.choices[0].message.content + ", Generate a name that matches the idea in 20 characters or less"}],
+        )
+        t2 = time.time()
+        total2 = t1-t0
+        print(completion.choices[0].message.content)
+        print(completion2.choices[0].message.content)
+        print(total1)
+        print(total2)
+        databaseInfo.addIdeaEntryQueryGenerated(completion2.choices[0].message.content, completion.choices[0].message.content, historyId)
+        generateStepByStep(completion2.choices[0].message.content, completion.choices[0].message.content, 1)
+    except  Exception as e:
+        exc = e
+
+    response_body = {
+        'message': exc
+    }
+    return response_body
+
+
+"""
+This endpoint is used to add an idea entry to the database. Gets called with an api call like so:
+http://127.0.0.1:5000/addIdeaEntry?IdeaName=testingAPI&Description=description%20of%20idea&HistoryID=b49d01e1-9d78-41bc-9a49-d037c4747a25
+"""
+@api.route('/addIdeaEntry', methods=['POST'])
+def addIdeaEntry():
+    historyId = request.json.get('HistoryId')
+    ideaName = request.json.get('IdeaName')
+    description = request.json.get('Description')
+    success = databaseInfo.addIdeaEntryQuery(description, ideaName, historyId)
+    generateStepByStep(ideaName, description, 1)
+    response_body = {
+        'message': str(success)
+    }
+    return response_body
+
+@api.route('/deleteIdeaEntry', methods=['POST'])
+def deleteIdeaEntry():
+    ideaEntryId = request.json.get('IdeaEntryId')
+    success = databaseInfo.deleteIdeaEntryQuery(ideaEntryId)
+    response_body = {
+        'message': str(success)
+    }
+    return response_body
+
+@api.route('/createNewHistory', methods=['POST'])
+def createNewHistory():
+    historyName = request.json.get('HistoryName')
+    username = request.json.get('userName')
+    success = databaseInfo.createNewHistory(historyName, username)
+    response_body = {
+        'message': "success of Create new history:" + str(success)
+    }
+    return response_body
+
+
+
+"""
+This endpoint is used to list all of the histories that we have.
+Called like so:
+http://127.0.0.1:5000/getAllHistory
+"""
+@api.route('/getAllHistory', methods=['GET'])
+def getAllHistory():
+    returnList = databaseInfo.getAllHistoryQuery()
+    response_body = {
+        'message': json.dumps(returnList, default=str, indent = 2)
+    }
+    return response_body 
     
-    return True
+"""
+This endpoint gets all of the ideas and its details for a specific history.
+Called like so:
+http://127.0.0.1:5000/getAllIdeaEntriesForHistory?HistoryID=b49d01e1-9d78-41bc-9a49-d037c4747a25
+"""
+@api.route('/getAllIdeaEntriesForHistory', methods=['GET'])
+def getAllIdeaEntriesForHistory():
+    historyID = request.args.get('HistoryID')
+    returnList = databaseInfo.getAllIdeaEntriesForHistory(historyID)
+    response_body = {
+        'message': json.dumps(returnList, default=str, indent = 2)
+    }
+    return response_body
 
-def addIdeaEntryQueryGenerated(ideaName, description, historyId):
-    conn = get_conn()
-    cursor = conn.cursor()
-    domain = str(uuid.uuid4())
-    try:
-        cursor.execute("INSERT INTO IdeaEntry (Name, Description, DomainName, HistoryId, IsGenerated) VALUES ( ?, ?, ?, ?, 1)", (ideaName, description, str(domain)[:20], historyId)) 
-        conn.commit()
-        conn.close()
-    except Exception as e:
-        print("An error occurred:", e)
-        return e 
-    
-    return True
+# Just an example on how to use the api
+def saveData(info):
+    #save info to DB
+    success = True
+    return success
 
+def accessData(info):
+    testResult = "test"
+    return testResult
 
-def deleteIdeaEntryQuery(ideaEntryId):
-    conn = get_conn()
-    cursor = conn.cursor()
-    domain = str(uuid.uuid4())
-    try:
-        cursor.execute("delete from IdeaEntry where IdeaEntryId = ?", (ideaEntryId)) 
-        conn.commit()
-        conn.close()
-    except Exception as e:
-        print("An error occurred:", e)
-        return e 
-    
-    return True
+def doComputation(info, name):
+    return info
 
-def createNewHistory(HistoryName, userName):
-    conn = get_conn()
-    cursor = conn.cursor()
-    domain = str(uuid.uuid4())
-    try:
-        cursor.execute("INSERT INTO HISTORY (Name, UserName, DomainName) VALUES (?, ?, ?)", (HistoryName, userName, str(domain)[:20]))
-        conn.commit()
-        conn.close()
-    except Exception as e:
-        print("An error occurred:", e)
-        return e 
-    return True
-
-def generate_custom_guid():
-    generated_uuid = uuid.uuid4()
-    return generated_uuid
-
-def get_conn():
-    conn = pyodbc.connect(connection_string)
-    return conn
-
-
-def getStepByStepForIdea(IdeaId):
-    returnList = []
-    conn = get_conn()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM StepByStepEntry WHERE IdeaEntryId=? ORDER BY StepNum", IdeaId)
-    columns = [column[0] for column in cursor.description]
-    for row in cursor.fetchall():
-        returnList.append(dict(zip(columns, row)))
-    conn.close()
-    return returnList
-
-def addStepByStepForIdea(IdeaEntryId, stepByStepJson, ideaId):
-    conn = get_conn()
-    cursor = conn.cursor()
-    domain = str(uuid.uuid4())
+def generateStepByStep(IdeaName, IdeaDescription, ideaId):
+    client = OpenAI(
+        api_key=""
+    )
 
     try:
-        data = json.dumps(stepByStepJson)
-        print('data: ')
-        print(stepByStepJson['result'])
-        for i in range(len(stepByStepJson['result'])):
-            string = '<a href ="' + stepByStepJson['result'][i]['link'] + '">' + stepByStepJson['result'][i]['step'] + '</a>'
-            print(string)
-            #cursor.execute("INSERT INTO StepByStepEntry (StepNum, Description, IdeaEntryId) VALUES ( ?, ?, ?)", (i, string, ideaId))
-        conn.commit()
-        conn.close()
+        print('Generating StepByStep')
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": "You are a new business entrepreneur, with your new business:" + IdeaName + ", which can be summarized as: " + IdeaDescription + ". Please provide a list of the first 10 steps you will take to make this business a reality. Each step should be max 10 words. This must be returned in this Schema: {'result': [{'step': The first step', 'link': 'useful link'}, {'step': 'The second step', 'link': 'useful link'}, {'step': 'third step', 'link': 'useful link'}]}"}]
+        )
+        print(completion.choices[0].message.content)
+        text = ""
+        for c in completion.choices[0].message.content:
+            if c == "'":
+                text+='"'
+            else:
+                text+=c
+        json_object = json.loads(text)
+        databaseInfo.addStepByStepForIdea(IdeaName, json_object, ideaId)
+    except  Exception as e:
+        print(e)
 
-    except Exception as e:
-        print("An error occurred:", e)
-        return e
 
-    return True
-
-#This can be changed to test the endpoints
-# def main():
-#     idea_entries = addIdeaEntryQuery("testing ideas2", "this is a test", "d0594ee2-5748-4da9-9cc2-3e606618c9a1")
-#     print(idea_entries)
-
-# if __name__ == "__main__":
-#     main()
